9978928f37ed8d2d1473ef31a8c768b5339c4c7a
jd_786 <jaydeep_shil@certree.com>
2022-11-11 13:04:20 +0530

    improved readable code

--- Partition to K Equal Sum Subsets/Program.cs
+++ Partition to K Equal Sum Subsets/Program.cs
@@ -7,7 +7,7 @@ namespace Partition_to_K_Equal_Sum_Subsets
   {
     static void Main(string[] args)
     {
-      var nums = new int[] { 5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3 }; // 4, 3, 2, 3, 5, 2, 1
+      var nums = new int[] { 4, 3, 2, 3, 5, 2, 1 }; //  //  5, 5, 5, 5, 4, 4, 4, 4, 3, 3, 3, 3
       int k = 4;
       Solution s = new Solution();
       var answer = s.CanPartitionKSubsets(nums, k);
@@ -20,34 +20,31 @@ namespace Partition_to_K_Equal_Sum_Subsets
   {
     public bool CanPartitionKSubsets(int[] nums, int k)
     {
-      // the sum of the nums should be divisible by k
-      if (nums.Sum() % k != 0) return false;
-      int target = nums.Sum() / k;
-      bool[] visited = new bool[nums.Length];
-      Array.Sort(nums, (a, b)=> { return b - a; });
-      bool BackTrack(int j, int k , int subsetSum)
+      var sum = nums.Sum();
+      if (sum % k != 0) return false;
+
+      int target = sum / k;
+      var visited = new bool[nums.Length];
+      return Backtrack(nums, k, 0, 0, target, visited);
+    }
+
+    private bool Backtrack(int[] nums, int k, int start, int sum, int target, bool[] visited)
+    {
+      if (k == 0) return true;
+      if (sum == target)
       {
-        // base condition 1
-        if (k == 0) return true;
-        // base condition 2
-        if(target == subsetSum)
-        {
-          // reduce the k by 1 and reset subset sum 0 and start again from the begining
-          return BackTrack(0, k - 1, 0);
-        }
-
-        for (int i = j; i < nums.Length; i++)
-        {
-          if (visited[i] || subsetSum + nums[i] > target) continue;
-          visited[i] = true;
-          if(BackTrack(i + 1, k, subsetSum + nums[i])) return true;
-          visited[i] = false; // usual backtracking template,,we need to set to false to make the index available for later
-        }
-
-        return false;
+        return Backtrack(nums, k - 1, 0, 0, target, visited);
       }
 
-      return BackTrack(0, k, 0);
+      for (int i = start; i < nums.Length; i++)
+      {
+        if (visited[i] || sum + nums[i] > target) continue;
+        visited[i] = true;
+        if (Backtrack(nums, k, i + 1, sum + nums[i], target, visited)) return true;
+        visited[i] = false;
+      }
+
+      return false;
     }
   }
 }
